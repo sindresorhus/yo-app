9a84a666768bf7da719a05a0533456394b44d74c
Sindre Sorhus <sindresorhus@gmail.com>
2014-01-11 03:33:54 +0100

    init

---
+++ main.js
@@ -0,0 +1,124 @@
+'use strict';
+var path = require('path');
+var gui = require('nw.gui');
+var yeoman = require('yeoman-generator');
+var _ = require('lodash');
+var _s = require('underscore.string');
+var findPkg = require('witwip');
+var stripAnsi = require('strip-ansi');
+var finderPath = require('finder-path');
+
+var TRAY_UPDATE_INTERVAL = 2000;
+
+
+
+function log(str) {
+	document.body.textContent = document.body.textContent + stripAnsi(str) + '\n';
+}
+
+process.stdout.write = log;
+process.stderr.write = log;
+
+
+function Adapter() {};
+
+Adapter.prototype.prompt = function () {
+	// TODO: figure out why it's not called
+	console.log('prompt', arguments);
+};
+
+Adapter.prototype.log = function () {};
+
+
+
+var env = yeoman(null, null, new Adapter());
+env.alias(/^([^:]+)$/, '$1:all');
+env.alias(/^([^:]+)$/, '$1:app');
+env.lookup();
+
+var generators = _.unique(env.namespaces().filter(function (el) {
+	return /(app|all)$/.test(el);
+}).map(function (el) {
+	return el.replace(/(\w+):\w+/, '$1');
+}));
+
+
+function createTrayMenu(name, generators, status) {
+	var menu = new gui.Menu();
+
+	menu.append(new gui.MenuItem({
+		label: name,
+		enabled: false
+	}));
+
+	menu.append(new gui.MenuItem({
+		type: 'separator'
+	}));
+
+	if (status) {
+		menu.append(new gui.MenuItem({
+			label: status,
+			enabled: false
+		}));
+
+		menu.append(new gui.MenuItem({type: 'separator'}));
+	}
+
+	generators.forEach(function (el) {
+		menu.append(new gui.MenuItem({
+			label: _s.capitalize(_s.humanize(el)),
+			click: function () {
+				try {
+					env.run(el + ':app');
+				} catch (err) {
+					log(err.message);
+				}
+			}
+		}));
+	});
+
+	menu.append(new gui.MenuItem({type: 'separator'}));
+
+	menu.append(new gui.MenuItem({
+		label: 'Quit',
+		click: function () {
+			gui.App.quit();
+		}
+	}));
+
+	return menu;
+}
+
+function updateTray() {
+	finderPath(function (err, dirPath) {
+		setTimeout(updateTray, TRAY_UPDATE_INTERVAL);
+
+		if (!dirPath) {
+			return;
+		}
+
+		findPkg(dirPath, function (err, pkgPath, pkgData) {
+			// package.json name or fallback to dir name
+			var name = pkgData && pkgData.name || path.basename(dirPath, path.extname(dirPath));
+			tray.menu = createTrayMenu(name, generators);
+		});
+	});
+}
+
+var win = gui.Window.get();
+var tray = new gui.Tray({
+	icon: 'menubar-icon@2x.png',
+	alticon: 'menubar-icon@2x.png'
+});
+
+var menu = new gui.Menu();
+menu.append(new gui.MenuItem({
+	label: 'No Yeoman project found',
+	enabled: false
+}));
+
+tray.menu = menu;
+updateTray();
+
+
+//win.showDevTools();
---
+++ readme.md
@@ -0,0 +1,24 @@
+# yo-app
+
+> Experimental [yo](https://github.com/yeoman/yo) app *(OS X)*
+
+![](screenshot.png)
+
+
+## Dev
+
+### Setup
+
+- Download [node-webkit](https://github.com/rogerwang/node-webkit#downloads) and put it in `/Applications`
+- Add `alias nw='/Applications/node-webkit.app/Contents/MacOS/node-webkit'` to your .bashrc/.zshrc
+
+### Run
+
+- `cd` into the app folder
+- Run `npm install` *(only first time)*
+- Run `nw .`
+
+
+## License
+
+MIT © [Sindre Sorhus](http://sindresorhus.com)
